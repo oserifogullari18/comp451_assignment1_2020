ad62c3f8afa6f8697a0e973345ede9ab4026be80
Alp ÖZASLAN <alpozaslan@Alps-MacBook-Pro.local>
2022-11-06 19:31:07 +0300

    clean up.

--- comp411/classifiers/softmax.py
+++ comp411/classifiers/softmax.py
@@ -2,7 +2,8 @@ import numpy as np
 from random import shuffle
 import builtins
 
-def softmax_loss_naive(W, X, y, reg_l2, reg_l1 = 0):
+
+def softmax_loss_naive(W, X, y, reg_l2, reg_l1=0):
     """
     Softmax loss function, naive implementation (with loops)
 
@@ -26,12 +27,12 @@ def softmax_loss_naive(W, X, y, reg_l2, reg_l1 = 0):
     # Initialize the loss and gradient to zero.
     loss = 0.0
     dW = np.zeros_like(W)
-    
+
     if reg_l1 == 0.:
         regtype = 'L2'
     else:
         regtype = 'ElasticNet'
-    
+
     ##############################################################################
     # TODO: Compute the softmax loss and its gradient using explicit loops.      #
     # Store the loss in loss and the gradient in dW. If you are not careful      #
@@ -41,14 +42,32 @@ def softmax_loss_naive(W, X, y, reg_l2, reg_l1 = 0):
     ##############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_train = X.shape[0]
+    num_classes = W.shape[1]
+    for i in range(num_train):
+        scores = X[i].dot(W)
+        scores -= np.max(scores)
+        correct_class_score = scores[y[i]]
+        exp_sum = np.sum(np.exp(scores))
+        loss += -correct_class_score + np.log(exp_sum)
+        for j in range(num_classes):
+            dW[:, j] += (np.exp(scores[j])/exp_sum)*X[i]
+        dW[:, y[i]] -= X[i]
+    loss /= num_train
+    dW /= num_train
+    if regtype == 'L2':
+        loss += reg_l2 * np.sum(W * W)
+        dW += 2*reg_l2*W
+    elif regtype == 'ElasticNet':
+        loss += reg_l2 * np.sum(W * W) + reg_l1 * np.sum(np.abs(W))
+        dW += 2*reg_l2*W + reg_l1*np.sign(W)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return loss, dW
 
 
-def softmax_loss_vectorized(W, X, y, reg_l2, reg_l1 = 0):
+def softmax_loss_vectorized(W, X, y, reg_l2, reg_l1=0):
     """
     Softmax loss function, vectorized version.
 
@@ -57,12 +76,12 @@ def softmax_loss_vectorized(W, X, y, reg_l2, reg_l1 = 0):
     # Initialize the loss and gradient to zero.
     loss = 0.0
     dW = np.zeros_like(W)
-    
+
     if reg_l1 == 0:
         regtype = 'L2'
     else:
         regtype = 'ElasticNet'
-    
+
     ##############################################################################
     # TODO: Compute the softmax loss and its gradient using no explicit loops.   #
     # Store the loss in loss and the gradient in dW. If you are not careful      #
@@ -72,7 +91,27 @@ def softmax_loss_vectorized(W, X, y, reg_l2, reg_l1 = 0):
     ##############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_train = X.shape[0]
+    num_classes = W.shape[1]
+    scores = X.dot(W)
+    scores -= np.max(scores, axis=1, keepdims=True)
+    exp_sum = np.sum(np.exp(scores), axis=1, keepdims=True)
+    correct_class_score = scores[np.arange(num_train), y]
+    loss = np.sum(-correct_class_score + np.log(exp_sum))
+    loss /= num_train
+    if regtype == 'L2':
+        loss += reg_l2 * np.sum(W * W)
+    elif regtype == 'ElasticNet':
+        loss += reg_l2 * np.sum(W * W) + reg_l1 * np.sum(np.abs(W))
+
+    softmax = np.exp(scores)/exp_sum
+    softmax[np.arange(num_train), y] -= 1
+    dW = X.T.dot(softmax)
+    dW /= num_train
+    if regtype == 'L2':
+        dW += 2*reg_l2*W
+    elif regtype == 'ElasticNet':
+        dW += 2*reg_l2*W + reg_l1*np.sign(W)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
